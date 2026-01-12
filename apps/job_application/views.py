0560d77403df0ed1436c51d80a7d2020bf08bf30
Justin-Ajith <justinajith11@gmail.com>
2026-01-12 00:33:54 +0000

    Update is applyed field

--- apps/job_application/views.py
+++ apps/job_application/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
 from apps.job_application.models import Company
 from apps.job_application.serializers import CompanySerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class JobApplicationViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+    @action(methods=['get'], detail=True, url_path='update-applyed')
+    def updateApplyedStatus(self, request, pk=None):
+        company = self.get_object()
+        company.is_apply = True
+        company.save()
+        return Response({'message': 'Job applyed successfuly!'}, status=200)
